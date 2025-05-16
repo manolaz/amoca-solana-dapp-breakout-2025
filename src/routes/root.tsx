6bd3482c39967971c064ad9052b24ce7260fa783
Tristan NGUYEN <trungupdate@gmail.com>
2025-05-17 03:33:44 +0700

    restore

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,26 +1,34 @@
-import { Box, Container, Flex, Heading, Text, Code } from '@radix-ui/themes';
-import { useState, Suspense } from 'react';
+import { Box, Code, Container, DataList, Flex, Heading, Spinner, Text, Button } from '@radix-ui/themes';
+import { getUiWalletAccountStorageKey } from '@wallet-standard/react';
+import { Suspense, useContext, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
-import { Sidebar } from '../components/Sidebar';
-import { HomePage } from '../components/HomePage';
+import { Balance } from '../components/Balance';
+import { FeatureNotSupportedCallout } from '../components/FeatureNotSupportedCallout';
+import { FeaturePanel } from '../components/FeaturePanel';
+import { SolanaSignAndSendTransactionFeaturePanel } from '../components/SolanaSignAndSendTransactionFeaturePanel';
+import { SolanaSignMessageFeaturePanel } from '../components/SolanaSignMessageFeaturePanel';
+import { WalletAccountIcon } from '../components/WalletAccountIcon';
+import { ChainContext } from '../context/ChainContext';
+import { SelectedWalletAccountContext } from '../context/SelectedWalletAccountContext';
+import { HomeSection } from '../components/HomeSection';
 import { InsurancePolicyPage } from '../components/InsurancePolicyPage';
-import { HomeHeader } from '../components/home/HomeHeader';
-import { WalletInfoSection } from '../components/wallet/WalletInfoSection';
-import { WalletFeatures } from '../components/wallet/WalletFeatures';
-import { getUiWalletAccountStorageKey } from '../utils/walletUtils';
-import { skyBlue } from '../theme/colors';
+import { Sidebar } from '../components/Sidebar';
+
+const skyBlue = '#38bdf8';
+const mintGreen = '#6ee7b7';
+const golden = '#fbbf24';
+
+const SIDEBAR_WIDTH = 220;
 
 function Root() {
+    const { chain } = useContext(ChainContext);
+    const [selectedWalletAccount] = useContext(SelectedWalletAccountContext);
     const [currentPage, setCurrentPage] = useState<'home' | 'policy'>('home');
-    const [selectedWalletAccount, setSelectedWalletAccount] = useState(null);
-    const [chain, setChain] = useState(null);
-    
     const errorBoundaryResetKeys = [
         chain,
         selectedWalletAccount && getUiWalletAccountStorageKey(selectedWalletAccount),
     ].filter(Boolean);
-    
     return (
         <Flex>
             <Sidebar currentPage={currentPage} setCurrentPage={setCurrentPage} />
@@ -28,37 +36,29 @@ function Root() {
                 <Container mx={{ initial: '3', xs: '6' }} style={{ background: '#f0fdfa', borderRadius: 24, paddingBottom: 32, marginLeft: 0 }}>
                     {currentPage === 'home' ? (
                         <>
-                            <HomeHeader />
-                            
+                            {/* AMOCA Mascot */}
+                            <Box
+                                as="img"
+                                src="/BLUE/a2.svg"
+                                alt="AMOCA Climate Guardian"
+                                style={{ display: 'block', margin: '16px auto', width: 150, height: 'auto' }}
+                            />
+                            <HomeSection />
                             <Box mt="8">
                                 {selectedWalletAccount ? (
                                     <Flex gap="6" direction="column">
-                                        <WalletInfoSection 
-                                            selectedWalletAccount={selectedWalletAccount}
-                                            chain={chain}
-                                        />
-                                        <WalletFeatures 
-                                            selectedWalletAccount={selectedWalletAccount}
-                                            errorBoundaryResetKeys={errorBoundaryResetKeys}
-                                        />
-                                    </Flex>
-                                ) : (
-                                    <Text as="p" align="center" mt="6" style={{ color: skyBlue, fontWeight: 600 }}>
-                                        Click &ldquo;Connect Wallet&rdquo; to get started.
-                                    </Text>
-                                )}
-                            </Box>
-                        </>
-                    ) : (
-                        <InsurancePolicyPage />
-                    )}
-                </Container>
-            </Box>
-        </Flex>
-    );
-}
-
-export default Root;
+                                        <Flex gap="2">
+                                            <Flex align="center" gap="3" flexGrow="1">
+                                                <WalletAccountIcon account={selectedWalletAccount} height="48" width="48" />
+                                                <Box>
+                                                    <Heading as="h4" size="3" style={{ color: skyBlue }}>
+                                                        {selectedWalletAccount.label ?? 'Unlabeled Account'}
+                                                    </Heading>
+                                                    <Code variant="outline" truncate size={{ initial: '1', xs: '2' }}>
+                                                        {selectedWalletAccount.address}
+                                                    </Code>
+                                                </Box>
+                                            </Flex>
                                             <Flex direction="column" align="end">
                                                 <Heading as="h4" size="3" style={{ color: golden }}>
                                                     Balance
